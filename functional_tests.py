1eba6a72f8721beb02e11b873658aa8a129f0d51
denibulkashvili <denibulkashvili@gmail.com>
2018-09-17 03:45:46 +0800

    Add a first FT class using unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome('D:/Projects/JobTrackBoard/chromedriver.exe')
-browser.get('localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Chrome('D:/Projects/JobTrackBoard/chromedriver.exe')
+
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Visitor enters a job-tack-board
+        self.browser.get('http://localhost:8000')
+
+        # Visitor sees a page title and header mention job-track-board
+        self.assertIn('JobTrackBoard', self.browser.title)
+        self.fail('Finish the test!')
+
+        # Visitor sees an option to add a job position
+        # other user stories
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
