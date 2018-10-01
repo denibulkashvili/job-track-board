a342f3b5dcb3345d6c1d12480b459edd0ec7124f
denibulkashvili <denibulkashvili@gmail.com>
2018-10-02 00:07:46 +0700

    Refactor home page view to use a template, change tests appopriately

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>Job Track Board</title></html>')
+    return render(request, "jobs/home.html")
