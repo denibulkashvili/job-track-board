82e78d7cb32c5a26cd4311ae2c258f3f40b40e86
denibulkashvili <denibulkashvili@gmail.com>
2018-10-04 22:52:34 +0700

    Generate homepage from template

--- functional_tests.py
+++ functional_tests.py
@@ -40,7 +40,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr') # Selenium return several elements with tag 'tr'
         self.assertTrue(
-            any(row.text == "Python Developer" for row in rows)
+            any(row.text == "1. Python Developer" for row in rows),
+            "New job position did not appear in table"
         )
     
         # other user stories
